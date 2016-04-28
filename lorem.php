24a27d6ece0616b6f4743254a2e905a8e88f156a
Etudiant <Etudiant>
2016-04-28 17:04:01 +0200

    création de lorem.txt

--- lorem.php
+++ lorem.php
@@ -1,7 +1,11 @@
 <?php
 require_once 'inc/config.php';
 
-$contents = nl2br(file_get_contents('text.txt'));
+$contents = nl2br(file_get_contents('lorem/text.txt'));
 $contents = explode('<br />'.PHP_EOL.'<br />'.PHP_EOL, $contents);
 
 debug($contents);
+
+foreach ($contents as $key => $content) {
+	file_put_contents('lorem/lorem'.$key.'.txt', $content);
+}
